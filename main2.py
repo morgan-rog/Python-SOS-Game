ad2f90714e9e9db91c0cb3a3b7407236d593bba5
Morgan Rogers <59988298+morgan-rog@users.noreply.github.com>
2021-10-11 14:45:00 -0500

    #1 completed GUI outline and add function to make a move on the board

--- main2.py
+++ main2.py
@@ -31,9 +31,6 @@ class SOS_GAME_BOARD():
             for col in range(self.NUM_COLS):
                 self.board[row].append(0)  # append empty cell
 
-    def set_cell_value(self, row, col, value):
-        self.board[row][col] = value
-
     def get_cell_value(self, row, col):
         return self.board[row][col]
 
@@ -51,20 +48,6 @@ class SOS_GAME_GUI():
     SIMPLE_GAME = 0
     GENERAL_GAME = 1
 
-
-    # red_player_label = Label(WINDOW, text='RED PLAYER')
-    # red_player_label.grid(row=2, column=9)
-
-    # blue_player_label = Label(WINDOW, text='BLUE PLAYER')
-    # blue_player_label.grid(row=2, column=10, padx=200)
-
-    # red_player_S = Radiobutton(WINDOW, text='S', variable= RED_PLAYER_OPTION, value='S').grid(row=3, column=9)
-    # red_player_O = Radiobutton(WINDOW, text='O', variable= RED_PLAYER_OPTION, value='O').grid(row=4, column=9)
-
-    # blue_player_S = Radiobutton(WINDOW, text='S', variable= BLUE_PLAYER_OPTION, value='S').grid(row=3, column=10, padx=200)
-    # blue_player_O = Radiobutton(WINDOW, text='O', variable= BLUE_PLAYER_OPTION, value='O').grid(row=4, column=10, padx=200)
-
-    
     def __init__(self):
         self.gameboard = SOS_GAME_BOARD()
         self.gametype = self.SIMPLE_GAME
@@ -77,23 +60,30 @@ class SOS_GAME_GUI():
     def create_GUI_gameboard(self):
         for r in range(self.gameboard.NUM_ROWS):
             for c in range(self.gameboard.NUM_COLS):
-                self.gameboard.board[r][c] = Button(self.WINDOW, bg="SystemButtonFace", height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, command=lambda: self.make_move(r,c))
-                self.gameboard.board[r][c].grid(row=r, column=c, padx= 2, pady=2)
+                tile = self.gameboard.board[r][c] = Button(self.WINDOW, bg="SystemButtonFace", height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, command=lambda row1 = r, col1 = c: self.make_move(row1, col1))
+                tile.grid(row=r, column=c, padx= 2, pady=2)
 
-        simple_game_button = Radiobutton(self.WINDOW, text='Simple Game', variable=self.gametype, value=self.SIMPLE_GAME, command=lambda: self.start_simple_game()).grid(row=0, column=9)
-        general_game_button = Radiobutton(self.WINDOW, text='General Game', variable=self.gametype, value=self.GENERAL_GAME, command=lambda: self.start_general_game()).grid(row=0, column=10)
+        # simple and general game buttons
+        simple_game_button = Radiobutton(self.WINDOW, text='Simple Game', variable=self.gametype, value=self.SIMPLE_GAME, command=lambda: self.start_simple_game())
+        simple_game_button.grid(row=0, column=9)
+        general_game_button = Radiobutton(self.WINDOW, text='General Game', variable=self.gametype, value=self.GENERAL_GAME, command=lambda: self.start_general_game())
+        general_game_button.grid(row=0, column=10)
 
+        # player labels
         red_player_label = Label(self.WINDOW, text='RED PLAYER')
         red_player_label.grid(row=2, column=9)
-
         blue_player_label = Label(self.WINDOW, text='BLUE PLAYER')
         blue_player_label.grid(row=2, column=10, padx=200)
 
-        red_player_S_button = Radiobutton(self.WINDOW, text='S', variable= self.RED_PLAYER_OPTION, value='S', command=lambda: self.radio_click()).grid(row=3, column=9)
-        red_player_O_button = Radiobutton(self.WINDOW, text='O', variable= self.RED_PLAYER_OPTION, value='O', command=lambda: self.radio_click()).grid(row=4, column=9)
-
-        blue_player_S_button = Radiobutton(self.WINDOW, text='S', variable= self.BLUE_PLAYER_OPTION, value='S', command=lambda: self.radio_click()).grid(row=3, column=10, padx=200)
-        blue_player_O_button = Radiobutton(self.WINDOW, text='O', variable= self.BLUE_PLAYER_OPTION, value='O', command=lambda: self.radio_click()).grid(row=4, column=10, padx=200)
+        # player S/O radio buttons
+        red_player_S_button = Radiobutton(self.WINDOW, text='S', variable= self.RED_PLAYER_OPTION, value='S', command=lambda: self.radio_click())
+        red_player_S_button.grid(row=3, column=9)
+        red_player_O_button = Radiobutton(self.WINDOW, text='O', variable= self.RED_PLAYER_OPTION, value='O', command=lambda: self.radio_click())
+        red_player_O_button.grid(row=4, column=9)
+        blue_player_S_button = Radiobutton(self.WINDOW, text='S', variable= self.BLUE_PLAYER_OPTION, value='S', command=lambda: self.radio_click())
+        blue_player_S_button.grid(row=3, column=10, padx=200)
+        blue_player_O_button = Radiobutton(self.WINDOW, text='O', variable= self.BLUE_PLAYER_OPTION, value='O', command=lambda: self.radio_click())
+        blue_player_O_button.grid(row=4, column=10, padx=200)
 
     def radio_click(self):
         print('Red value: ', self.RED_PLAYER_OPTION.get())
@@ -107,8 +97,13 @@ class SOS_GAME_GUI():
 
     def make_move(self, row, col):
         print('row: ', row, ' col: ', col)
-        button_pressed = self.gameboard.board[row][col]
-        button_pressed['text'] = 'S'
+        tile = self.gameboard.get_cell_value(row, col)
+        if self.RED_PLAYER_OPTION.get() == 'S':
+            tile['text'] = 'S'
+        elif self.RED_PLAYER_OPTION.get() == 'O':
+            tile['text'] = 'O'
+            
+        
 
 
 
